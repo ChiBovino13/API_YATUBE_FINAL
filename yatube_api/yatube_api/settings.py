ce1279ae205b7c48011d460225e631cceb0e9362
Дарья Горячева <75489718+ChiBovino13@users.noreply.github.com>
2023-02-16 22:49:23 +0300

    Yatube_API_Final Исправлено по isort

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -17,8 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'djoser',
-    'django_filters',
     'api',
     'posts',
 ]
